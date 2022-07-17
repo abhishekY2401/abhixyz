63b120c3d12ab3c6edb405dfc07eda5dc03515f5
abhishekY2401 <abhiyd2401@gmail.com>
2022-07-17 22:09:37 +0530

    restyled the portfolio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Navbar from "./Navbar";
 import Projects from "./Projects";
+import Hashnode from "./images/Hashnode";
 
 function App() {
   return (
@@ -9,51 +10,73 @@ function App() {
       <main>
         <div className="names">
           <div className="about-me-card">
-            <h1>💻 About me</h1>
-            <div className="dash"></div>
             <h4>
-              Hey, there 👋 <br />
-              I'm Abhishek Yadav - Aspiring Software and Product Engineer
+              <p>Hi, there!</p>
               <br />
-              Currently, I am in second year of my university, <br /> pursuing
-              Bachelors of Engineering in IT.
-              <br />I have experience in building websites using different
-              technologies <br />
-              with Python and JavaScript libraries and MySQL database, having{" "}
-              <br />a main interest in backend development.
+              I'm Abhishek currently a third year undergraduate, <br />
+              aspiring software engineer having and making in <br />
+              passion for backend development, always trying to <br /> make the
+              server secure, robust and handle any types
+              <br /> of request. My aim is to produce good quality apps
+              <br />
+              and websites. I love building and designing products.
             </h4>
-          </div>
-          <div className="view-work">
-            <h1>View my work</h1>
-            <br />
-            <a href="/">
-              <i class="bi bi-arrow-down-circle"></i>
-            </a>
           </div>
         </div>
       </main>
 
       <section>
         <div className="social-links">
-          <a href="https://github.com/abhishekY2401/" className="github">
+          <a
+            target="_blank"
+            href="https://github.com/abhishekY2401/"
+            className="github"
+            rel="noreferrer"
+          >
             <i class="bi bi-github"></i>
           </a>
           <a
+            target="_blank"
             href="https://www.linkedin.com/in/abhishek-yadav-1a1808205/"
             className="linkedin"
+            rel="noreferrer"
           >
             <i class="bi bi-linkedin"></i>
           </a>
-          <a href="https://twitter.com/AbhiY2852" className="twitter">
+          <a
+            target="_blank"
+            href="https://twitter.com/AbhiY2852"
+            className="twitter"
+            rel="noreferrer"
+          >
             <i class="bi bi-twitter"></i>
           </a>
-          <a href="/" className="arrow">
-            <i class="bi bi-arrow-right"></i>
+          <a
+            target="_blank"
+            href="https://abhi2048.hashnode.dev/"
+            className="hashnode"
+            rel="noreferrer"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              width="25"
+              height="25"
+            >
+              <path
+                fill="#fff"
+                d="M35.19 171.1C-11.72 217.1-11.72 294 35.19 340.9L171.1 476.8C217.1 523.7 294 523.7 340.9 476.8L476.8 340.9C523.7 294 523.7 217.1 476.8 171.1L340.9 35.19C294-11.72 217.1-11.72 171.1 35.19L35.19 171.1zM315.5 315.5C282.6 348.3 229.4 348.3 196.6 315.5C163.7 282.6 163.7 229.4 196.6 196.6C229.4 163.7 282.6 163.7 315.5 196.6C348.3 229.4 348.3 282.6 315.5 315.5z"
+              />
+            </svg>
           </a>
         </div>
       </section>
 
-      <Projects />
+      <div className="footer">
+        <p>
+          <span>&#169;</span> 2022 Abhishek Yadav.
+        </p>
+      </div>
     </div>
   );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,32 +1,33 @@
 import "./Navbar.css";
+import Sunlight from "./images/Sunlight";
 
 const Navbar = () => {
   return (
     <div className="navbar">
-      <div>
-        <h1 id="my-logo">Abhi. Y</h1>
+      <div id="logo">
+        <h1 id="my-logo">
+          <span>-</span>a<span>-</span>
+        </h1>
       </div>
       <nav>
         <ul>
-          <li className="work">
-            <i class="bi bi-briefcase"></i>
-            <a href="/">Work Experience</a>
-          </li>
           <li className="projects">
-            <i class="bi bi-code-square"></i>
-            <a href="/">Projects</a>
+            <a href="/" id="pro">
+              Projects
+            </a>
           </li>
           <li className="blog">
-            <i class="bi bi-file-earmark"></i>
-            <a href="/">Blogs</a>
-          </li>
-          <li className="about">
-            <i class="bi bi-file-person"></i>
-            <a href="/">About Me</a>
+            <a href="https://abhi2048.hashnode.dev/" id="blogs">
+              Blogs
+            </a>
           </li>
           <li className="contact">
-            <i class="bi bi-envelope"></i>
-            <a href="/">Contact Me</a>
+            <a href="/" id="me">
+              Contact Me
+            </a>
+          </li>
+          <li>
+            <Sunlight class="sun" />
           </li>
         </ul>
       </nav>
